37f3677e0169368ec63a4b7f25d02f4d00bf3291
caa1982 <cyrilalvarezadriaansen@gmail.com>
2018-05-16 15:21:04 +0200

    fixed test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,12 +3,5 @@ const Token = artifacts.require("./Token2GB.sol");
 const trezorAccount = '0x40A1c574cFeC7D8Bc03B6Ca0d739972596f37F31'
 
 module.exports = function(deployer) {
-<<<<<<< HEAD
-  deployer.deploy(Token, trezorAccount)
-    //.then(()=>{
-    //  return deployer.deploy(Mock, Token.address)
-    //});
-=======
     deployer.deploy(Token, trezorAccount)
->>>>>>> b014c45196af6ba62b7f59fe69700a55093a0d28
 };
